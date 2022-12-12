26405e4a555a937324599aadf8d3d32298b94723
Francesco Buda - francesco.buda3@studio.unibo.it <francesco.buda3@studio.unibo.it>
2022-12-12 11:11:00 +0100

    modificati User.cs e SocialNetworkUser.cs

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -8,7 +8,6 @@ namespace Collections
     {
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
         }
 
         public bool AddFollowedUser(string group, TUser user)
@@ -20,7 +19,8 @@ namespace Collections
         {
             get
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                //throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                return FollowedUsers;
             }
         }
 
--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -22,7 +22,23 @@ namespace Collections
         public string Username { get; }
 
         public bool IsAgeDefined => Age.HasValue;
-        
+
         // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   Age == user.Age &&
+                   FullName == user.FullName &&
+                   Username == user.Username &&
+                   IsAgeDefined == user.IsAgeDefined;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Age, FullName, Username, IsAgeDefined);
+        }
+
+        public override string ToString() => $"FullName: {FullName} Username: {Username} Age: {(IsAgeDefined ? Age : "not defined")}";
     }
 }
